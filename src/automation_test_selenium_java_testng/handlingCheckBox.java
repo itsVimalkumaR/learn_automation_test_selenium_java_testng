f9552feeb1efc2091b40d4c0c24f76241f5c6027
ItsVimalkumarM <126799548+itsVimalkumaR@users.noreply.github.com>
2026-04-15 14:23:05 +0530

    just

--- src/automation_test_selenium_java_testng/handlingCheckBox.java
+++ src/automation_test_selenium_java_testng/handlingCheckBox.java
@@ -3,7 +3,9 @@ package automation_test_selenium_java_testng;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class handlingCheckBox {
 
